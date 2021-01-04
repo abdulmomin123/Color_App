f0606dd2b1f86e4979dee576b3157f1440a43a7c
Abdul Momin <abdulmomin114477@gmail.com>
2021-01-05 05:42:02 +0600

    Ad palette list comp and basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
 import { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import PaletteList from './PaletteList';
 import Palette from './Palette';
 import { palettes } from './seedColor';
 import { generatePalette } from './colorHelpers';
 
 class App extends Component {
+  findPalette = (id: string) => palettes.find(palette => palette.id === id);
+
   render = () => {
     return (
       <div>
-        <Palette palette={generatePalette(palettes[1])} />
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => <PaletteList palettes={palettes} />}
+          />
+
+          <Route
+            exact
+            path="/palette/:id"
+            render={rProps => (
+              <Palette
+                palette={generatePalette(
+                  this.findPalette(rProps.match.params.id)!
+                )}
+              />
+            )}
+          />
+        </Switch>
       </div>
     );
   };
--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Slider from 'rc-slider';
 import Snackbar from '@material-ui/core/Snackbar';
 import IconButton from '@material-ui/core/IconButton';
@@ -34,7 +35,7 @@ export default class Navbar extends Component<{
     return (
       <header className="Navbar">
         <div className="logo">
-          <a href="/">reactcolorpicker</a>
+          <Link to={'/'}>React Color Picker</Link>
         </div>
         <div className="slider-container">
           <span>Level: {level}</span>
